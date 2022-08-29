875c60d28815ddabc2c40716daba894212ba9640
0xMoJo7 <ericd@simpli.fi>
2022-08-28 21:57:56 -0500

    Adds Arrow.sol example and README explanation

--- contracts/Arrow.sol
+++ contracts/Arrow.sol
@@ -11,22 +11,42 @@ contract Arrow is ISnowV1Program {
         return "Arrow";
     }
 
+    function reverse(uint256 x) public pure returns (uint256) {
+        x = (x & 0x5555555555555555555555555555555555555555555555555555555555555555) << 1 |
+            (x & 0xAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA) >> 1;
+        x = (x & 0x3333333333333333333333333333333333333333333333333333333333333333) << 2 |
+            (x & 0xCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC) >> 2;
+        x = (x & 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F) << 4 |
+            (x & 0xF0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0) >> 4;
+        x = (x & 0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF) << 8 |
+            (x & 0xFF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00) >> 8;
+        x = (x & 0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF) << 16 |
+            (x & 0xFFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000) >> 16;
+        x = (x & 0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF) << 32 |
+            (x & 0xFFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000) >> 32;
+        x = (x & 0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF) << 64 |
+            (x & 0xFFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF0000000000000000) >> 64;
+        x = (x & 0x00000000000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) << 128 |
+            (x & 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000000000000000000000000000) >> 128;
+        return x;
+    }
+
     function run(uint256[64] memory canvas, uint8 lastIndex)
         external
         returns (uint8 index, uint256 value)
     {
         uint256[2] memory sprites = [
             // Sprites created with https://snow.computer/operators
-            0xe000f000f8007c003e001f000f8007c003e001f000f8007c003e001f000f0007,
-            0xe000f000f8007c003e001f000f8007c103e301f700ff007f003f007f00ff01ff
+            0xe000f000f8007c003e001f000f8007c003e001f000f8007c003e001f000f0007, // Diagonal Line
+            0xe000f000f8007c003e001f000f8007c103e301f700ff007f003f007f00ff01ff  // Downwards Arrow down 
         ];
 
-        if (canvas[61] != sprites[1]) {
+        if (canvas[61] != sprites[1]) { // Downright Arrow
             return (61, sprites[1]);
-        } else if (canvas[52] != sprites[0]) {
+        } else if (canvas[43] != reverse(sprites[1])) { // Upsidedown Arrow
+            return (43, reverse(sprites[1]));
+        } else if (canvas[52] != sprites[0]) { // Diagonal Line
             return (52, sprites[0]);
-        } else if (canvas[43] != sprites[0]) {
-            return (43, sprites[0]);
         } 
     }
 }
